4e08d20b9d4a59f6c1135f0ea4e9e4e247fefd60
Clombardi91 <chl9195@icloud.com>
2021-05-26 20:43:39 -0400

    updated visuals, added button to create new todo, new issue with editing previous todo

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Todo
 from django.shortcuts import render, get_object_or_404
 from .forms import TodoForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -16,7 +17,33 @@ def todo_list(request):
 def todo_detail(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     return render(request, 'main/todo_detail.html', {'todo': todo})
+    
 
 def todo_new(request):
     form = TodoForm()
-    return render(request, 'main/todo_edit.html', {'form': form})   
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.text = request.user
+            todo.created_date = timezone.now()
+            todo.save()
+            return redirect('todo_detail', pk=todo.pk)
+    else:
+        form = TodoForm()
+    return render(request, 'main/todo_edit.html', {'form': form})
+
+def todo_edit(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.text = request.user
+            todo.created_date = timezone.now()
+            todo.save()
+            return redirect('todo_detail', pk=todo.pk)
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'main/todo_edit.html', {'form': form})
+     
